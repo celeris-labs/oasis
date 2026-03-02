cf7090afa54419b0e70924379e70689ff342f01a
Luca <luca@teapot.ovh>
2026-03-02 09:40:26 +0100

    update software alongside parcore

--- software/oasis/parcore/rdma_reader.cpp
+++ software/oasis/parcore/rdma_reader.cpp
@@ -13,19 +13,19 @@ RDMAReader::RDMAReader(
     std::shared_ptr<libstf::MemoryPool> memory_pool,
     std::shared_ptr<libstf::TLBManager> tlb_manager,
     std::shared_ptr<libstf::OutputBufferManager> output_buffer_manager,
-    RDMAReadConfig rdma_config,
-    ::parcore::ColumnChunkDecoderConfig column_chunk_config,
-    ::parcore::PageDecoderConfig page_config,
+    std::shared_ptr<::parcore::ColumnChunkDecoderConfig> column_chunk_config,
+    std::shared_ptr<::parcore::PageDecoderConfig> page_config,
+    std::shared_ptr<RDMAReadConfig> rdma_config,
     const ::parcore::metadata::Metadata &meta, uintptr_t memory_offset,
     libstf::stream_t stream)
     : HardwareReader(cthread, memory_pool, tlb_manager, output_buffer_manager,
                      column_chunk_config, page_config, meta, stream),
-      config(rdma_config), offset(memory_offset), stream(stream) {}
+      config_(rdma_config), offset_(memory_offset) {}
 
 void RDMAReader::send_page(const ::parcore::metadata::Page &page,
                            ::parcore::PageType page_type) {
   Profiler::open_regions({prefix + "send_page"});
-  config.read(stream, offset + page.offset, page.size);
+  config_->read(decoder_, offset_ + page.offset, page.size);
   Profiler::close_regions({prefix + "send_page"});
 }
 
--- software/oasis/parcore/rdma_reader.hpp
+++ software/oasis/parcore/rdma_reader.hpp
@@ -11,20 +11,20 @@ namespace parcore {
 
 class RDMAReader : public ::parcore::fpga::HardwareReader {
 private:
-  RDMAReadConfig config;
-  uintptr_t offset;
-  libstf::stream_t stream;
+  std::shared_ptr<RDMAReadConfig> config_;
+  uintptr_t offset_;
 
 public:
-  RDMAReader(std::shared_ptr<coyote::cThread> cthread,
-             std::shared_ptr<libstf::MemoryPool> memory_pool,
-             std::shared_ptr<libstf::TLBManager> tlb_manager,
-             std::shared_ptr<libstf::OutputBufferManager> output_buffer_manager,
-             RDMAReadConfig rdma_config,
-             ::parcore::ColumnChunkDecoderConfig column_chunk_config,
-             ::parcore::PageDecoderConfig page_config,
-             const ::parcore::metadata::Metadata &meta, uintptr_t offset,
-             libstf::stream_t stream = 0);
+  RDMAReader(
+      std::shared_ptr<coyote::cThread> cthread,
+      std::shared_ptr<libstf::MemoryPool> memory_pool,
+      std::shared_ptr<libstf::TLBManager> tlb_manager,
+      std::shared_ptr<libstf::OutputBufferManager> output_buffer_manager,
+      std::shared_ptr<::parcore::ColumnChunkDecoderConfig> column_chunk_config,
+      std::shared_ptr<::parcore::PageDecoderConfig> page_config,
+      std::shared_ptr<RDMAReadConfig> rdma_config,
+      const ::parcore::metadata::Metadata &meta, uintptr_t offset,
+      libstf::stream_t stream = 0);
 
 private:
   void send_page(const ::parcore::metadata::Page &page,
