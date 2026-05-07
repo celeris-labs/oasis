a24b71e8e09fdc34c49c7950d4ba832460e693e8
Jonas Dann <jonas.dann@inf.ethz.ch>
2026-05-07 23:19:46 +0200

    Changed Oasis system id to 0x0A515

--- hardware/src/hdl/common.sv
+++ hardware/src/hdl/common.sv
@@ -1,6 +1,6 @@
 package oasis;
 
-parameter longint unsigned OASIS_SYSTEM_ID = 64'h44320bcab5312b33;
+parameter longint unsigned OASIS_SYSTEM_ID = 64'h0A515;
 
 parameter int NUM_RDMA_READ_CONFIG_REGS = 2;
 parameter longint unsigned RDMA_READ_CONFIG_ID = 64'h2f966a70f04c0e93;
